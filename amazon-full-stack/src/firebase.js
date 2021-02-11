9441efe86d2de735fb59da4d01c6c1761fde310f
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-02-11 17:40:14 -0300

    Cabeçalho da aplicação.

---
+++ amazon-full-stack/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAwHYFFX08B_sc3VbITmXi3i0OeO8YEzaw",
+  authDomain: "clone-c74b2.firebaseapp.com",
+  projectId: "clone-c74b2",
+  storageBucket: "clone-c74b2.appspot.com",
+  messagingSenderId: "41600339693",
+  appId: "1:41600339693:web:20377c929990b625c7c6e4",
+  measurementId: "G-VLWT2PTTWZ"
+};

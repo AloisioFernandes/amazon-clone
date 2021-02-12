0307afb46f00db3bc3f440ab63427f09c84ac6a1
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-02-12 20:30:22 -0300

    Estrutura do componente SubTotal.

--- amazon-full-stack/src/Checkout/Checkout.js
+++ amazon-full-stack/src/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Checkout.css'
 
+import Subtotal from '../Subtotal/Subtotal'
+
 function Checkout() {
   return (
     <div className="checkout">
@@ -13,7 +15,7 @@ function Checkout() {
       </div>
 
       <div className="checkout__right">
-        <h2>The subtotal will go here</h2>
+        <Subtotal />
       </div>
     </div>
   )

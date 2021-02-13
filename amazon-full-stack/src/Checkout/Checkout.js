33978d93fa06f66287ba4b7e638e0f5036b53c9f
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-02-13 18:28:41 -0300

    Adição de produtos no carrinho de compras.

--- amazon-full-stack/src/Checkout/Checkout.js
+++ amazon-full-stack/src/Checkout/Checkout.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Checkout.css'
+import { useStateValue } from '../StateProvider'
 
 import Subtotal from '../Subtotal/Subtotal'
+import CheckoutProduct from '../CheckoutProduct/CheckoutProduct'
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue() // useContext(StateContext)
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -11,6 +15,16 @@ function Checkout() {
 
         <div>
           <h2 className="checkout__title">Your shopping Basket</h2>
+
+          {basket.map(item => (
+            <CheckoutProduct 
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
 

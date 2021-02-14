b56f3e08565cbc02e41f623f7f9eaa0f8b66b6b4
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-02-14 19:00:59 -0300

    Página de Sign In concluída.

--- amazon-full-stack/src/App.js
+++ amazon-full-stack/src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
 import Header from './Header/Header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import Home from './Home/Home'
 import Checkout from './Checkout/Checkout'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './Login/Login'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
 
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
